4bcf886dd42ac9e65e4c72513fa6b196be51d060
Zuoming <1920274083@qq.com>
2024-07-11 11:10:42 +0800

    add a helloworld file

---
+++ build/CMakeFiles/HELLO.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HELLO.dir/main.o"
+  "CMakeFiles/HELLO.dir/main.o.d"
+  "HELLO"
+  "HELLO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HELLO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
